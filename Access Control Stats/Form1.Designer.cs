4b84fb4e0b28ad31a899ad44e1686b0e127343f8
Andreas Aakvik Gogstad <agogstad@gmail.com>
2022-10-06 13:37:21 +0200

    Add project files.

---
+++ Access Control Stats/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Access_Control_Stats
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.RichTextBox();
+            this.checkFullList = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(93, 16);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(38, 15);
+            this.labelFileName.TabIndex = 1;
+            this.labelFileName.Text = "label1";
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(12, 60);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(782, 457);
+            this.textResult.TabIndex = 3;
+            this.textResult.Text = "";
+            // 
+            // checkFullList
+            // 
+            this.checkFullList.AutoSize = true;
+            this.checkFullList.Location = new System.Drawing.Point(13, 39);
+            this.checkFullList.Name = "checkFullList";
+            this.checkFullList.Size = new System.Drawing.Size(66, 19);
+            this.checkFullList.TabIndex = 4;
+            this.checkFullList.Text = "Full List";
+            this.checkFullList.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 556);
+            this.Controls.Add(this.checkFullList);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private Button buttonOpenFile;
+        private Label labelFileName;
+        private RichTextBox textResult;
+        private CheckBox checkFullList;
+    }
+}
---
+++ Access Control Stats/Form1.cs
@@ -0,0 +1,146 @@
+using Microsoft.VisualBasic;
+
+namespace Access_Control_Stats
+{
+    public partial class Form1 : Form
+    {
+
+        private List<string> date;
+        private List<string> name;
+        private List<DateAndUser> dateAnduserList = new List<DateAndUser>();
+        private DateList dateList = new DateList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+            string fileName = openFileDialog1.FileName;
+            labelFileName.Text = fileName;
+            parseCSV(fileName);
+            outputResult();
+        }
+
+        private void parseCSV(string fileName)
+        {
+            StreamReader reader = new StreamReader(fileName);
+            //date = new List<string>();
+            //name = new List<string>();
+
+            var skip = reader.ReadLine(); // skip first line
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line != null)
+                {
+                    string[] values = line.Split(';');
+
+                    string dateText = values[0].Trim('"');
+                    DateTime newDate = DateTime.ParseExact(dateText, "yyyy-MM-dd HH:mm:ss", null);
+                    string shortDate = newDate.Date.ToString();
+
+                    string newName = values[1];
+
+                    //date.Add(shortDate);
+                    //name.Add(newName);
+
+                    dateAnduserList.Add(new DateAndUser(shortDate, newName));
+                }
+                else
+                {
+                    labelFileName.Text = "StreamReader error";
+                }
+            }
+        }
+
+        private void outputResult()
+        {
+            int maxLines = 100000;  // TEST, REMOVE LATER!!! <-----------------------------------------------------
+            textResult.Text = "Lines in the CSV: " + dateAnduserList.Count + "\n";
+
+            //List<string> namesInDate = new List<string>();
+
+            for (int i = 0; i < dateAnduserList.Count && i < maxLines; i++)
+            {
+                dateList.AddUser(dateAnduserList[i].date, dateAnduserList[i].name);    
+                
+                //textResult.Text += dateAnduserList[i].date + " : " + dateAnduserList[i].name + "\n";             
+            }
+            textResult.Text += "\n-------------------\n";
+            foreach (KeyValuePair<string, UserList> entry in dateList.dates)
+            {
+                textResult.Text += entry.Key.Substring(0,10) + " : " + entry.Value.names.Count +  "\n";
+                if (checkFullList.Checked)
+                {
+                    foreach (string n in entry.Value.names)
+                    {
+                        textResult.Text += "   " + n + "\n";
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public class DateAndUser
+    {
+        public DateAndUser (string date, string name)
+        {
+            this.date = date;
+            this.name = name;
+        }
+
+        public string date;
+        public string name;
+    }
+
+    public class UserList
+    {
+        public string date;
+        public List<string> names;
+
+        public UserList(string date)
+        {
+            this.date = date;
+            this.names = new List<string>();
+        }
+
+        public void Add(string name)
+        {
+            if (!names.Contains(name))
+            {
+                names.Add(name);
+            }
+        }
+    }
+
+    public class DateList
+    {
+        public List<UserList> dateList = new List<UserList> ();
+        //private List<string> dates = new List<string>();
+        public Dictionary<string, UserList> dates = new Dictionary<string, UserList> ();
+
+        public void Add(string date)
+        {
+            //if (!dates.ContainsKey)
+            if (!dates.ContainsKey(date))
+            {
+                dates.Add(date, new UserList(date));
+                //dateList.Add(new UserList(date));
+            }
+        }
+
+        public void AddUser(string date, string name)
+        {
+            if (!dates.ContainsKey(date))
+            {
+                Add(date);
+            }
+            dates[date].Add(name);
+        }
+    }
+}
